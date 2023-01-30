8832feeeff92e886fadf2f5384b4421c76dd9d29
Temitope joshua <ojotemitopeidnoble10@gmail.com>
2023-01-30 20:54:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Page from './page/Page';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+main:{
+  bgBlack:' #1E1E1E',
+  navColor:'#161215'
+}
+
+ 
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<ThemeProvider theme={theme}>
+<Page/>
+
+</ThemeProvider>
+
+</>
   );
 }
 
