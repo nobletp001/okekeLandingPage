0fda174865841629e062fea40d49eee66da1da0b
Temitope joshua <ojotemitopeidnoble10@gmail.com>
2023-01-31 21:28:47 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Button,  Stack, Typography, useTheme} from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 function Header() {
     const theme = useTheme()
+    const [closeNav, setcloseNav] = React.useState(false);
   return (
-    <Stack  direction='row' alignItems="center" justifyContent="space-between" sx={{
+<>
+<Stack  direction='row' alignItems="center" justifyContent="space-between" sx={{
         height:'90px',
         width:'100%',
         backgroundColor:theme.main.navColor,
@@ -70,6 +73,7 @@ sx={{
         xs:'none'
     }
 }}>Features</Typography>
+
 <Button   variant="outlined" sx={{
    border:'1px solid rgba(255, 255, 255, 0.21)',
  display:{
@@ -94,17 +98,110 @@ Launch Dapp
     </Typography>
     </Button>
 
-<MenuIcon  sx={{
-    color:'#fff',
-    fontSize:'2.5rem',
+{
+    !closeNav && <MenuIcon 
+    onClick={()=>{
+        setcloseNav(!closeNav)
+    }}
+    sx={{
+        color:'#fff',
+        fontSize:'2.5rem',
+        display:{
+            xs:'inline-block',
+            md:'none'
+        },
+        cursor:'pointer'
+    }}/>
+    
+}
+{
+    closeNav  && <CloseIcon
+    onClick={()=>{
+        setcloseNav(!closeNav)
+    }}
+    sx={{
+        color:'#fff',
+        fontSize:'2.5rem',
+        display:{
+            xs:'inline-block',
+            md:'none'
+        },
+        cursor:'pointer'
+    }}/>
+    
+}
+    </Stack>
+    
+ <Stack sx={{
+    width:'100%',
+    height:'auto',
     display:{
-        xs:'inline-block',
+        xs:closeNav ?'flex':'none',
         md:'none'
     },
-    cursor:'pointer'
-}}/>
+    justifyContent:'center',
+    flexDirection:'column' ,
+    alignItems:'center',
+    backgroundColor:theme.main.navColor,
+    borderRadius:'20px',
+    animation: closeNav ? '5s ease 5s slidein' :'5s linear 20s slideout'
+   
 
-    </Stack>
+ }}>
+<Typography 
+sx={{
+    fontSize:'20px',
+    lineHeight:'24px',
+    color:'#fff',
+    fontWeight:'400',
+    cursor:'pointer',
+    padding:2
+}}>About</Typography>
+
+<Typography 
+sx={{
+    fontSize:'20px',
+    lineHeight:'24px',
+    color:'#fff',
+    fontWeight:'400',
+    cursor:'pointer',
+    padding:2
+}}>Roadmap</Typography>
+<Typography 
+sx={{
+    fontSize:'20px',
+    lineHeight:'24px',
+    color:'#fff',
+    fontWeight:'400',
+    cursor:'pointer',
+    padding:2
+}}>Features</Typography>
+
+<Button   variant="outlined" sx={{
+   border:'1px solid rgba(255, 255, 255, 0.21)',
+
+    ":hover":{
+        border:'1px solid rgba(255, 255, 255, 0.21)',
+    },
+height:'43px',
+borderRadius:'8px',
+margin:2
+}}>
+    <Typography  sx={{
+    fontSize:'20px',
+    lineHeight:'24px',
+    color:'#fff',
+    fontWeight:'400',
+   textTransform:'capitalize',
+   backgroundColor:"transparent",
+}}>
+Launch Dapp
+    </Typography>
+    </Button>
+ </Stack>
+
+ 
+</>
   )
 }
 
